75891cb0d11e46f712c67ca06eba1d903f224cc6
shedboy71 <webmaster@ihservices.co.uk>
2022-04-02 14:59:50 +0100

    Add files via upload

---
+++ code example/oddeven/oddnumbers.py
@@ -0,0 +1,7 @@
+# Print Odd Numbers from minimum to maximum
+minimum = int(input(" Please Enter the Minimum Value : "))
+maximum = int(input(" Please Enter the Maximum Value : "))
+
+for number in range(minimum, maximum+1):
+    if(number % 2 != 0):
+        print("{0}".format(number))
