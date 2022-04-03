8bdb0bf7043a6e1bc6b90aa9f8ba75bf8800ee2f
shedboy71 <webmaster@ihservices.co.uk>
2022-04-03 22:18:23 +0100

    Add files via upload

---
+++ code example/String examples/stringlower1.py
@@ -0,0 +1,6 @@
+myText1 = input("Please Enter your Own Text : ")
+
+myText2 = myText1.lower()
+ 
+print("\nOriginal  =  ", myText1)
+print("Result =  ", myText2)
---
+++ code example/String examples/stringupper1.py
@@ -0,0 +1,6 @@
+myText1 = input("Please Enter your Own Text : ")
+
+myText2 = myText1.upper()
+ 
+print("\nOriginal  =  ", myText1)
+print("Result =  ", myText2)
