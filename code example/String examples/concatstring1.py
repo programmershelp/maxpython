89dadcb2c8b693ad82d2d24298ac39305d6b695a
shedboy71 <webmaster@ihservices.co.uk>
2022-04-03 21:58:36 +0100

    Add files via upload

---
+++ code example/String examples/concatstring1.py
@@ -0,0 +1,6 @@
+# Concatenate Strings
+myString1 = input("Please Enter the First  String : ")
+myString2 = input("Please Enter the Second String : ")
+
+concatstring = myString1 + ' ' + myString2
+print("Concatenated Strings = ", concatstring)
---
+++ code example/String examples/concatstring2.py
@@ -0,0 +1,6 @@
+# Python Program to Concatenate Strings
+concatstring1 = 'This ' 'is ' 'a ' 'test'
+print("Concatenate String 1 = ", concatstring1)
+
+concatstring2 = ('This ' 'is ' 'a ' 'test')
+print("Concatenate String 2 = ", concatstring2)
