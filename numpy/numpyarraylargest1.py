6e66e28c6d4ca7f161ad32f76df030cc6b76992f
shedboy71 <webmaster@ihservices.co.uk>
2022-04-03 12:27:41 +0100

    Add files via upload

---
+++ numpy/numpyarraylargest1.py
@@ -0,0 +1,4 @@
+import numpy as np
+myarray = np.array([5, 32, 17, 56, 98, 23, 36, 33, 45, 92])
+print("Array Items = ", myarray)
+print("The Largest Number in the Array = ", max(myarray))
---
+++ numpy/numpyarraylargest2.py
@@ -0,0 +1,6 @@
+import numpy as np
+myarray = np.array([5, 32, 17, 56, 98, 23, 36, 33, 45, 92])
+print("Array Items = ", myarray)
+myarray.sort()
+arraylength = len(myarray) - 1
+print("The Largest Number is = ", myarray[arraylength])
